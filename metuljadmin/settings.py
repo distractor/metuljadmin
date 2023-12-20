91958cd8f1dc04d0e1ef8ec01d4589d81ec6d33b
Mitja Jancic <mitjajancic@gmail.com>
2023-12-20 16:56:36 +0100

    Link fix.

--- metuljadmin/settings.py
+++ metuljadmin/settings.py
@@ -100,21 +100,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'metuljadmin.wsgi.application'
 
 # Database
-# if IS_PRODUCTION:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.environ.get('DB_NAME'),
-#             'USER': os.environ.get('DB_USER'),
-#             'PASSWORD': os.environ.get('DB_PASSWORD'),
-#             'HOST': os.environ.get('DB_HOST'),
-#             'PORT': os.environ.get('DB_PORT')
-#         }
-#     }
-# else:
-# DATABASES = {
-#     "default": dj_database_url.config(default=os.environ.get('DATABASE_URL')),
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get('DB_NAME', BASE_DIR / "db.sqlite3"),
+        'USER': os.environ.get('DB_USER', "user"),
+        'PASSWORD': os.environ.get('DB_PASSWORD', "password"),
+        'HOST': os.environ.get('DB_HOST', "localhost"),
+        'PORT': os.environ.get('DB_PORT', "5432")
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
