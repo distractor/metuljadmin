749275de03ab695c5acc1df4d3ad169847180fc4
Mitja Jancic <mitjajancic@gmail.com>
2023-12-20 16:07:23 +0100

    Link fix.

--- metuljadmin/settings.py
+++ metuljadmin/settings.py
@@ -102,11 +102,14 @@ WSGI_APPLICATION = 'metuljadmin.wsgi.application'
 # Database
 if IS_PRODUCTION:
     DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT')
+        }
     }
 else:
     DATABASES = {
